cb3abcd43552e8e9e46b6455593cc2fa00939a80
AndreiAgmata <agmataa1124@gmail.com>
2023-10-08 01:38:28 -0400

    Updated get-by-id functionality

--- src/routes/api/get-by-id.js
+++ src/routes/api/get-by-id.js
@@ -1,6 +1,6 @@
 const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
-const { createErrorResponse } = require('../../response');
+const { createErrorResponse, createSuccessResponse } = require('../../response');
 
 /**
  * Get a fragment by ID
@@ -8,10 +8,10 @@ const { createErrorResponse } = require('../../response');
 module.exports = async (req, res) => {
   try {
     const fragment = await Fragment.byId(req.user, req.params.id);
-
     const data = await fragment.getData();
     res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send(data);
+    //res.status(200).send(data);
+    res.status(200).json(createSuccessResponse({ data }));
   } catch (err) {
     logger.error({ err }, 'Unable to find a fragment with matching ID');
     res.status(404).json(createErrorResponse(404, 'Unable to find fragment'));
